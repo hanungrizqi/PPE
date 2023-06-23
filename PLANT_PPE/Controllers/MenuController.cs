f9e7ff0b675cac8559718395fe6574015da8db94
Hanung Rizqi Widianto <hanung.widianto@kppmining.com>
2023-06-23 10:48:13 +0700

    initial commit

--- PLANT_PPE/Controllers/MenuController.cs
+++ PLANT_PPE/Controllers/MenuController.cs
@@ -31,11 +31,11 @@ namespace PLANT_PPE.Controllers
                 else if (Session["PositionID"].ToString() == "KP1PT02") {
                     ViewBag.Sub = db.TBL_R_SUB_MENUs.Where(x => x.Akses.Contains("Plant Manager")).ToList();
                 }
-                else if (Session["PositionID"].ToString() == "SP1D1" || Session["PositionID"].ToString() == "TM1Q1" || Session["PositionID"].ToString() == "RT1D01" || Session["PositionID"].ToString() == "BD1D10" || Session["PositionID"].ToString() == "AS1Q1" || Session["PositionID"].ToString() == "IN1AB1" || Session["PositionID"].ToString() == "TO1Q1" || Session["PositionID"].ToString() == "SR1Q1" || Session["PositionID"].ToString() == "IN1AC1" || Session["PositionID"].ToString() == "AG1Q1")
+                else if (Session["PositionID"].ToString() == "SP1D1" || Session["PositionID"].ToString() == "TM1Q1" || Session["PositionID"].ToString() == "RT1D01" || Session["PositionID"].ToString() == "BD1D1" || Session["PositionID"].ToString() == "AS1Q1" || Session["PositionID"].ToString() == "IN1AB1" || Session["PositionID"].ToString() == "TO1Q1" || Session["PositionID"].ToString() == "SR1Q1" || Session["PositionID"].ToString() == "IN1AC1" || Session["PositionID"].ToString() == "AG1Q1")
                 {
                     ViewBag.Sub = db.TBL_R_SUB_MENUs.Where(x => x.Akses.Contains("Plant Dept Head")).ToList();
                 }
-                else if (Session["PositionID"].ToString() == "BD11" || Session["PositionID"].ToString() == "AS11" || Session["PositionID"].ToString() == "IN11" || Session["PositionID"].ToString() == "TO11" || Session["PositionID"].ToString() == "RT11" || Session["PositionID"].ToString() == "SP11")
+                else if (Session["PositionID"].ToString() == "BD11" || Session["PositionID"].ToString() == "AS11" || Session["PositionID"].ToString() == "IN11" || Session["PositionID"].ToString() == "TO11" || Session["PositionID"].ToString() == "RT11" || Session["PositionID"].ToString() == "SP11" || Session["PositionID"].ToString() == "SR11" || Session["PositionID"].ToString() == "PE11")
                 {
                     ViewBag.Sub = db.TBL_R_SUB_MENUs.Where(x => x.Akses.Contains("PM Pengirim") || x.Akses.Contains("PM Penerima")).ToList();
                 }
